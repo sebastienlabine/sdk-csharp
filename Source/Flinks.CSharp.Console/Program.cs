6762423e450e676846275efd51fc82ece8e27921
Gean <ggpedro@flinks.io>
2019-01-20 09:23:03 -0500

    - hello world

---
+++ Source/Flinks.CSharp.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Flinks.CSharp.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+            System.Console.ReadLine();
+        }
+    }
+}
