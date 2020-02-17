ad526e712ecbd01a3de9f9a7c214b5e5c3dbcbc2
Sebastien Labine <sebastien@smartbills.ca>
2020-02-17 18:22:31 -0500

    Modified Institution Enum to be usable as a class property

--- Source/Flinks.CSharp.SDK.Model/Enums/Institution.cs
+++ Source/Flinks.CSharp.SDK.Model/Enums/Institution.cs
@@ -3,7 +3,7 @@
 // - file, You can obtain one at https://mozilla.org/MPL/2.0/.
 namespace Flinks.CSharp.SDK.Model.Enums
 {
-    public static class Institution
+    public class Institution
     {
         public const string ATB = "ATB";
         public const string BMO = "BMO";
